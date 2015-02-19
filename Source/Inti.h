b1a7a06e3b2e6863f006d0eeeae1a5b2962769a3
Elvis Nuñez <elvisnunez@me.com>
2015-02-20 00:05:56 +0100

    Add dream API

--- Source/Inti.h
+++ Source/Inti.h
@@ -1,5 +1,14 @@
 @import Foundation;
+@import CoreData;
 
 @interface Inti : NSObject
+
+- (void)fetchItemsFromEntity:(NSString *)entityName completion:(void (^)(NSError *error))completion;
+
+- (void)saveObject:(NSManagedObject *)object completion:(void (^)(NSManagedObject *insertedObject, NSError *error))completion;
+
+- (void)updateObject:(NSManagedObject *)object completion:(void (^)(NSManagedObject *updatedObject, NSError *error))completion;
+
+- (void)deleteObject:(NSManagedObject *)object completion:(void (^)(NSError *error))completion;
 
 @end
--- Source/Inti.m
+++ Source/Inti.m
@@ -2,4 +2,25 @@
 
 @implementation Inti
 
+- (void)fetchItemsFromEntity:(NSString *)entityName completion:(void (^)(NSError *error))completion
+{
+
+}
+
+- (void)saveObject:(NSManagedObject *)object completion:(void (^)(NSManagedObject *insertedObject, NSError *error))completion
+{
+
+}
+
+- (void)updateObject:(NSManagedObject *)object completion:(void (^)(NSManagedObject *updatedObject, NSError *error))completion
+{
+
+}
+
+- (void)deleteObject:(NSManagedObject *)object completion:(void (^)(NSError *error))completion
+{
+
+}
+
+
 @end
